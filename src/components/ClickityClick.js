c73e5544d4b068791fa2258bd81815d410465a29
Soumya_Goyal <somyagoyal739@gmail.com>
2019-11-21 09:41:48 -0800

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,25 @@
-// Code ClickityClick Component Here
+import React, { Component } from 'react'
+
+export class ClickityClick extends Component {
+    constructor(){
+        super();
+        this.state = {
+            count: 0
+        }
+    }
+
+    handleClick = () =>{
+        this.setState(prviousState => {return{count: previousState.count + 1}})
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.handleClick}>Click me!</button>
+            </div>
+        )
+    }
+}
+
+export default ClickityClick
